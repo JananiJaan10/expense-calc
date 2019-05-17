95f7bfc66c7a5dba3a06f1c85eff6f9dca0c4516
JananiJaan10 <49401550+JananiJaan10@users.noreply.github.com>
2019-05-17 14:37:12 +0530

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>expense page</title>
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">XPENS CALC </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login">Login</a>
+      </li>
+     
+      
+    </ul>
+    
+  </div>
+</nav>
+    </header>
+</head>
+<body>
+  
+  <h1>XPENS CALC</h1>
+<div class="card-center" style="width: 18rem;">
+  <img src="https://cdn0.iconfinder.com/data/icons/conceptual-flat-icons-1/200/vector_210-252_04-512.png" class="" alt="Xpens calc ">
+  <div class="card-body">
+    <h5 class="card-title-center">Xpens Calc</h5>
+    <p class="card text-center">An expense is the cost of operations that a company incurs to generate revenue. As the popular saying goes, “it costs money to make money.”</p>
+    <a href="#" class="btn btn-primary">read</a>
+  </div>
+</div>
+</body>
+</html>
